f011d337769dbcb85ac575a26d4155ec3a18204e
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-11-14 19:03:46 +0100

    📦️ Add RDP code

---
+++ src/PyRDP/RamerDouglasPeucker.h
@@ -0,0 +1,18 @@
+#ifndef RDP
+#define RDP
+
+#include <vector>
+
+namespace rdp {
+struct Point2D
+{
+    double x;
+    double y;
+};
+
+void RamerDouglasPeucker(const std::vector<Point2D> &pointList, std::size_t startIndex,
+                         std::size_t endIndex, double epsilonSquared,
+                         std::vector<std::size_t> &indicesToKeep);
+}
+
+#endif
