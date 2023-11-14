c690fd8e4cb2f74c5b561a816c2f87ff775ae579
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-11-14 19:00:22 +0100

    🎨  Clean wrapper

--- src/PyRDP/wrapper.cpp
+++ src/PyRDP/wrapper.cpp
@@ -1,35 +1,72 @@
+#include <iostream>
 #include <Python.h>
 #include <numpy/arrayobject.h>
+#include "RamerDouglasPeucker.h"
 
-double mysum(size_t n, double *X) {
-    double s = 0.0;
-    for (size_t i = 0; i < n; ++i) {
-        s += X[i];
-    }
-    return s;
-}
-
-static PyObject* mysum_wrapper(PyObject* self, PyObject* args) {
-    PyObject* arr_obj;
-    if (!PyArg_ParseTuple(args, "O", &arr_obj)) {
+static PyObject* myfun_wrapper(PyObject* self, PyObject* args) {
+    PyObject* arr1_obj;
+    PyObject* arr2_obj;
+    double param;
+    
+    if (!PyArg_ParseTuple(args, "OOd", &arr1_obj, &arr2_obj, &param)) {
         return NULL;
     }
 
-    PyArrayObject* arr = reinterpret_cast<PyArrayObject*>(arr_obj);
-    if (!PyArray_ISCARRAY(arr)) {
-        PyErr_SetString(PyExc_TypeError, "Input array must be contiguous C-style");
+    PyArrayObject* arr1 = reinterpret_cast<PyArrayObject*>(arr1_obj);
+    PyArrayObject* arr2 = reinterpret_cast<PyArrayObject*>(arr2_obj);
+
+    if (!PyArray_ISCARRAY(arr1) || !PyArray_ISCARRAY(arr2)) {
+        PyErr_SetString(PyExc_TypeError, "Input arrays must be contiguous C-style");
         return NULL;
     }
 
-    double* data = static_cast<double*>(PyArray_DATA(arr));
-    npy_intp n = PyArray_SIZE(arr);
+    double* data1 = static_cast<double*>(PyArray_DATA(arr1));
+    double* data2 = static_cast<double*>(PyArray_DATA(arr2));
+    npy_intp nPoints = PyArray_SIZE(arr1);
 
-    double result = mysum(n, data);
-    return PyFloat_FromDouble(result);
+    std::vector<rdp::Point2D> points;
+    points.reserve(nPoints);
+    for (npy_intp i = 0; i < nPoints; i++) {
+        points.push_back({data1[i], data2[i]});
+    }
+
+    // Create vectors from the input NumPy arrays
+    std::vector<size_t> indicesToKeep;
+    indicesToKeep.reserve(nPoints);
+    indicesToKeep.push_back(0);
+
+    rdp::RamerDouglasPeucker(points, 0, nPoints - 1, param * param, indicesToKeep);
+
+    // Create new NumPy arrays to return
+    std::size_t nIndices = indicesToKeep.size();
+
+    // npy_intp dims[1] = {nIndices};
+    npy_intp dims[1] = {static_cast<npy_intp>(nIndices)};
+    // npy_intp dims[1];
+    // dims[0] = nIndices;
+
+    PyObject* result1_obj = PyArray_FROM_OTF(PyArray_SimpleNew(1, dims, NPY_DOUBLE), NPY_DOUBLE, NPY_ARRAY_FORCECAST);
+    PyObject* result2_obj = PyArray_FROM_OTF(PyArray_SimpleNew(1, dims, NPY_DOUBLE), NPY_DOUBLE, NPY_ARRAY_FORCECAST);
+
+    if (!result1_obj || !result2_obj) {
+        Py_XDECREF(result1_obj);
+        Py_XDECREF(result2_obj);
+        return NULL;
+    }
+
+    double* result1_data = static_cast<double*>(PyArray_DATA(reinterpret_cast<PyArrayObject*>(result1_obj)));
+    double* result2_data = static_cast<double*>(PyArray_DATA(reinterpret_cast<PyArrayObject*>(result2_obj)));
+
+    for (size_t i = 0; i < nIndices; ++i) {
+        result1_data[i] = data1[indicesToKeep[i]];
+        result2_data[i] = data2[indicesToKeep[i]];
+    }
+
+    return Py_BuildValue("OO", result1_obj, result2_obj);
 }
 
 static PyMethodDef module_methods[] = {
-    {"mysum", mysum_wrapper, METH_VARARGS, "Calculate the sum of elements in a NumPy array."},
+    {"myfun", myfun_wrapper, METH_VARARGS, "Calculate the sum of elements in a NumPy array."},
     {NULL, NULL, 0, NULL}
 };
 
@@ -42,7 +79,8 @@ static struct PyModuleDef module_def = {
 };
 
 PyMODINIT_FUNC PyInit_RDP(void) {
-    import_array();  // Initialize NumPy
+    // Initialize NumPy
+    import_array(); 
     return PyModule_Create(&module_def);
 }
 
