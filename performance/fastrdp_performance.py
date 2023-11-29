61b8fe41798e1a7d8970b54ca64c86098fb3eade
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2023-11-29 19:53:37 +0100

    ⚡️ Measure performance differently

--- performance/fastrdp_performance.py
+++ performance/fastrdp_performance.py
@@ -1,13 +1,38 @@
-import pyperf
+import os
+from timeit import timeit
+from datetime import datetime
 
-from fastrdp import rdp
+import polars as pl
 import numpy as np
 
-if __name__ == '__main__':
-    size = 10_000
+from fastrdp import rdp
 
-    x = np.random.rand(size)
-    y = np.random.rand(size)
+max_exp = 6
+max_size = pow(10, max_exp)
+x = np.random.rand(max_size)
+y = np.random.rand(max_size)
 
-    runner = pyperf.Runner()
-    runner.timeit('fastrdp', 'result = rdp(x, y, 0.06)', globals=locals())
+exponents = list(range(4, max_exp + 1))
+
+execution_times = []
+for exp in exponents:
+    size = pow(10, exp)
+    this_x = x[0:size]
+    this_y = y[0:size]
+    repetitions = 1 if exp >= 6 else 10
+
+    total_time = timeit(lambda: rdp(this_x, this_y, 0.1), number=repetitions)
+    average_time = total_time / repetitions
+    execution_times.append(average_time)
+
+
+execution_df = pl.DataFrame({
+    'Name': 'fastrdp-python',
+    'Version': 'foo',
+    'Exponents': exponents,
+    'ExecutionTime': execution_times,
+    'TimeOfExecution': datetime.now()
+})
+
+save_path = os.path.join(os.path.dirname(__file__), 'execution_time.csv')
+execution_df.write_csv(save_path)
