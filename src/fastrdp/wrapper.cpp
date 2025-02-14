6cdba3ea57730b660e0efe1f88f16809764eedfe
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2025-02-14 21:14:30 +0100

    ✨ New method to get indices

--- src/fastrdp/wrapper.cpp
+++ src/fastrdp/wrapper.cpp
@@ -5,6 +5,43 @@
 
 namespace py = pybind11;
 
+py::array_t<size_t> rdp_index_wrapper(py::array_t<double> array1, py::array_t<double> array2, double epsilon)
+{
+    if (epsilon < 0.0)
+        throw std::domain_error("epsilon must be non-negative");
+
+    py::buffer_info buf1 = array1.request(), buf2 = array2.request();
+
+    // Make sure the input arrays have the correct shape and data type
+    if (buf1.ndim != 1 || buf2.ndim != 1)
+        throw std::domain_error("Inputs should be vectors");
+
+    auto nPoints = buf1.size;
+    if (nPoints != buf2.size)
+        throw std::length_error("Inputs have different lengths");
+
+    // if (nPoints <= 2)
+    //     return std::make_pair(array1, array2);
+
+    std::vector<double> vec1((double *)buf1.ptr, (double *)buf1.ptr + buf1.size);
+    std::vector<double> vec2((double *)buf2.ptr, (double *)buf2.ptr + buf2.size);
+
+    // Prepare input for RDP function
+    std::vector<rdp::Point2D> points;
+    points.reserve(nPoints);
+    for (auto i = 0; i < nPoints; i++)
+        points.push_back({vec1[i], vec2[i]});
+
+    std::vector<size_t> indicesToKeep;
+    indicesToKeep.reserve(nPoints);
+    indicesToKeep.push_back(0);
+
+    rdp::RamerDouglasPeucker(points, 0, nPoints - 1, epsilon * epsilon, indicesToKeep);
+
+    // return indicesToKeep;
+    return py::array_t<size_t>(indicesToKeep.size(), indicesToKeep.data());
+}
+
 std::pair<py::array_t<double>, py::array_t<double>> rdp_wrapper(py::array_t<double> array1, py::array_t<double> array2, double epsilon)
 {
     if (epsilon < 0.0)
@@ -56,6 +93,13 @@ std::pair<py::array_t<double>, py::array_t<double>> rdp_wrapper(py::array_t<doub
 
 PYBIND11_MODULE(fastrdp, m)
 {
+    m.def("rdp_index", &rdp_index_wrapper, R"mydelimiter(
+        rdp_index(x, y, epsilon)
+
+        The input is a curve sampled at the points `(x[i], y[i])` from NumPy vectors `x` and `y`.
+        Select a subset of the points as a coarser approximation using the Ramer-Douglas-Peucker algorithm with tolerance `epsilon`.
+        Returns the indices of the elements in `x` and `y` that are kept.
+)mydelimiter");
     m.def("rdp", &rdp_wrapper, R"mydelimiter(
         rdp(x, y, epsilon)
 
