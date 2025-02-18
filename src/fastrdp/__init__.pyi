8330ce95cf68cb3b8f4b8d08c05d0af3a77417d8
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2025-02-18 12:28:55 +0100

    📝 Add type hints for IDE

---
+++ src/fastrdp/__init__.pyi
@@ -0,0 +1,22 @@
+from typing import Sequence
+
+import numpy as np
+import numpy.typing as npt
+
+def rdp(x: Sequence[float], y: Sequence[float], epsilon: float) -> tuple[npt.NDArray[np.floating], npt.NDArray[np.floating]]:
+    """
+    rdp_index(x, y, epsilon)
+
+    The input is a curve sampled at the points `(x[i], y[i])` from `x` and `y`.
+    Select a subset of the points as a coarser approximation using the Ramer-Douglas-Peucker algorithm with tolerance `epsilon`.
+    """
+    ...
+
+def rdp_index(x: Sequence[float], y: Sequence[float], epsilon: float) -> npt.NDArray[np.int_]:
+    """
+    rdp_index(x, y, epsilon)
+
+    The input is a curve sampled at the points `(x[i], y[i])` from `x` and `y`.
+    Returns the indices of the elements that are kept in an approximation using the Ramer-Douglas-Peucker algorithm with tolerance `epsilon`.
+    """
+    ...
