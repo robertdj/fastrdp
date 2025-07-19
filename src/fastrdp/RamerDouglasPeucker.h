01afe465138845c36521344549b3860cd7b12caa
Max <max.lindqvist@ipp.mpg.de>
2025-07-19 14:42:45 +0200

    Added templates to src/fastrdp/RamerDouglasPeucker.h

--- src/fastrdp/RamerDouglasPeucker.h
+++ src/fastrdp/RamerDouglasPeucker.h
@@ -1,17 +1,23 @@
 #ifndef RDP
 #define RDP
 
+#include <array>
 #include <vector>
+#include <cstddef>
 
 namespace rdp {
-struct Point3D
-{
-    double x;
-    double y;
-    double z;
+template <std::size_t N>
+struct Point {
+    std::array<double, N> data{};
+
+    Point() { data.fill(0.0); }
+    Point(std::initializer_list<double> vals) {
+        std::copy(vals.begin(), vals.end(), data.begin());
+    }
 };
 
-void RamerDouglasPeucker(const std::vector<Point3D> &pointList, std::size_t startIndex,
+template <std::size_t N>
+void RamerDouglasPeucker(const std::vector<Point<N> &pointList, std::size_t startIndex,
                          std::size_t endIndex, double epsilonSquared,
                          std::vector<std::size_t> &indicesToKeep);
 }
