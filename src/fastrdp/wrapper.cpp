f7a4b35b2ea5752e6634428cc8dc6d3fc478d2ae
Max <max.lindqvist@ipp.mpg.de>
2025-07-19 16:16:45 +0200

    Cleanup

--- src/fastrdp/wrapper.cpp
+++ src/fastrdp/wrapper.cpp
@@ -19,11 +19,11 @@ rdp_index(const std::array<py::array_t<double>, N> &arrays, double epsilon)
 
     // Make sure the input arrays have the correct shape and data type
     auto nPoints = buf[0].size;
-    for (std::size_t i = 1; i < N; ++i)
+    for (std::size_t i = 0; i < N; ++i)
         if (buf[i].ndim != 1)
             throw std::invalid_argument("Inputs should be vectors");
 
-        if (nPoints != buf[i].size)
+        if (i > 0 && bufs[i].size != bufs[0].size)
             throw std::length_error("Inputs have different lengths");
     if (nPoints <= 2)
     {
