fb1fa6fdc1e3d35cd950fe531567d3e6546f0846
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2025-08-10 12:28:08 +0200

    🧐 Measure constructor

--- performance/measure_performance.cpp
+++ performance/measure_performance.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, char* argv[])
 {
-    const std::size_t n = (argc > 1) ? static_cast<std::size_t>(std::atoll(argv[1])) : 100;
+    const std::size_t n = (argc > 1) ? static_cast<std::size_t>(std::atoll(argv[1])) : 10000000;
     printf("n: %zu \n", n);
     double epsilon = 0.5;
 
@@ -18,11 +18,22 @@ int main(int argc, char* argv[])
     std::mt19937 g(rd());
     std::uniform_real_distribution<double> u(-10.0, 10.0);
 
+    std::vector<double> x(n);
+    std::vector<double> y(n);
+    for (size_t i = 0; i != n; i++) {
+        x[i] = u(g);
+        y[i] = u(g);
+    }
+
+    auto time_start_points = std::chrono::steady_clock::now();
     std::vector<rdp::Point<2>> points;
     points.reserve(n);
     for (size_t i = 0; i != n; i++) {
-        points.push_back({ u(g), u(g) });
+        points.push_back({ x[i], y[i] });
     }
+    auto time_end_points = std::chrono::steady_clock::now();
+    std::chrono::duration<double> duration_points = time_end_points - time_start_points;
+    printf("duration points: %g s\n", duration_points.count());
 
     std::vector<size_t> indicesToKeep;
     indicesToKeep.reserve(n);
@@ -32,6 +43,5 @@ int main(int argc, char* argv[])
     rdp::RamerDouglasPeucker(points, 0, n - 1, epsilon * epsilon, indicesToKeep);
     auto time_end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration = time_end - time_start;
-    printf("duration: %g s\n", duration.count());
-
+    printf("duration RDP: %g s\n", duration.count());
 }
